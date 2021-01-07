172ba06beb152b4aa8e8b16a3da4b189f1180210
Philippo Rodrigues <philipporodrigues@Philippos-MacBook-Pro.local>
2021-01-07 20:09:26 -0300

    Update

---
+++ src/test/java/br/com/caelum/ingresso/validacao/GerenciadorDeSessaoTest.java
@@ -0,0 +1,12 @@
+package br.com.caelum.ingresso.validacao;
+
+import org.aspectj.lang.annotation.Before;
+
+public class GerenciadorDeSessaoTest {
+	
+//	@Before(value = "")
+//	public void preparaSessoes() {
+//		
+//	}
+
+}
