eea31de1b06ba61ce9ef305e9513471b5db6e25c
Philippo Rodrigues <philipporodrigues@Philippos-MacBook-Pro.local>
2021-01-14 22:55:22 -0300

    Implementando pgto, compra e limpando carrinho

--- src/main/java/br/com/caelum/ingresso/controller/CompraController.java
+++ src/main/java/br/com/caelum/ingresso/controller/CompraController.java
@@ -1,14 +1,20 @@
 package br.com.caelum.ingresso.controller;
 
+import br.com.caelum.ingresso.dao.CompraDao;
 import br.com.caelum.ingresso.dao.LugarDao;
 import br.com.caelum.ingresso.dao.SessaoDao;
 import br.com.caelum.ingresso.model.Carrinho;
+import br.com.caelum.ingresso.model.Cartao;
 import br.com.caelum.ingresso.model.form.CarrinhoForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @Controller
 public class CompraController {
@@ -22,6 +28,9 @@ public class CompraController {
     @Autowired
     private Carrinho carrinho;
 
+    @Autowired
+    private CompraDao compraDao;
+
     @PostMapping("/compra/ingressos")
     public ModelAndView enviarParaPagamento(CarrinhoForm carrinhoForm){
         ModelAndView mv = new ModelAndView("redirect:/compra");
@@ -32,10 +41,26 @@ public class CompraController {
     }
 
     @GetMapping("/compra")
-    public ModelAndView checkout(){
+    public ModelAndView checkout(Cartao cartao){
         ModelAndView mv = new ModelAndView("compra/pagamento");
 
         mv.addObject("carrinho", carrinho);
+        return mv;
+    }
+
+    @PostMapping("/compra/comprar")
+    @Transactional
+    public ModelAndView comprar(@Valid Cartao cartao, BindingResult result){
+        ModelAndView mv = new ModelAndView("redirect:/");
+
+        if (cartao.isValido()) {
+            compraDao.save(carrinho.toCompra());
+            this.carrinho.limpa();
+        } else{
+            result.rejectValue("vencimento", "Vencimento inválido!");
+            return checkout(cartao);
+        }
+
         return mv;
     }
 }
